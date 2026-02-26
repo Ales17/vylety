5536afbf1564b491119d90adba8e4334e86e1ef7
AlesBacul <alesbacul@gmail.com>
2026-02-26 20:31:20 +0000

    refactor: rename date prop to dateCreated in PageWrapper component

--- frontend/components/PageWrapper.tsx
+++ frontend/components/PageWrapper.tsx
@@ -3,13 +3,17 @@ import { formatDate } from "@/lib/strings";
 interface Props {
   children: React.ReactNode;
   pageName: string;
-  date?: string;
+  dateCreated?: string;
 }
-export default function PageWrapper({ children, pageName, date }: Props) {
+export default function PageWrapper({ children, pageName, dateCreated
+ }: Props) {
   return (
     <div className=" border rounded-3xl bg-white border-slate-200 p-2 md:p-4">
       <h1 className="text-4xl mb-2">{pageName}</h1>
-      {date && <p className="mb-2">{formatDate(date)}</p>}
+      {dateCreated
+ && <p className="mb-2">{formatDate(dateCreated
+
+      )}</p>}
       {children}
     </div>
   );
