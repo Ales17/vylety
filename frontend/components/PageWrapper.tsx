39735b455e06315f05f8fd97adf2264a7217f776
AlesBacul <alesbacul@gmail.com>
2026-02-26 21:09:18 +0000

    refactor: restructure DateInfo component and update PageWrapper to use it for date display

--- frontend/components/PageWrapper.tsx
+++ frontend/components/PageWrapper.tsx
@@ -4,16 +4,24 @@ interface Props {
   children: React.ReactNode;
   pageName: string;
   dateCreated?: string;
+  dateUpdated?: string;
 }
-export default function PageWrapper({ children, pageName, dateCreated
+
+function DateInfo({ dateCreated, dateUpdated }: { dateCreated: string; dateUpdated: string }) {
+    return (
+        <p className="mb-2">
+            {formatDate(dateCreated)} {(dateUpdated !== dateCreated) && <>(aktualizováno {formatDate(dateUpdated)})</>}
+        </p>
+    );
+}
+
+
+export default function PageWrapper({ children, pageName, dateCreated, dateUpdated
  }: Props) {
   return (
     <div className=" border rounded-3xl bg-white border-slate-200 p-2 md:p-4">
       <h1 className="text-4xl mb-2">{pageName}</h1>
-      {dateCreated
- && <p className="mb-2">{formatDate(dateCreated
-
-      )}</p>}
+      {(dateCreated && dateUpdated) && <DateInfo dateCreated={dateCreated} dateUpdated={dateUpdated} />}
       {children}
     </div>
   );
